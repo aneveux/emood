71f7c8968d17fb04b33d865887f22732bcb400ab
Antoine Neveux <antoine.neveux@gmail.com>
2012-07-13 00:17:18 +0200

    Added package-info for javadoc purpose

---
+++ emood.plugin/src/main/java/fr/antoineneveux/emood/ui/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package aims at containing all the ui related objects. From the Eclipse views to 
+ * wizards used to get some inputs from the user, all the UI related components will be 
+ * included in this package.
+ * 
+ * @author Antoine Neveux
+ * @version 1.0
+ */
+package fr.antoineneveux.emood.ui;
